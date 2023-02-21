80f5801f1d46e2f1eccd7fd01a33841db3c553a3
h0ngcha0 <lbs.lhc@gmail.com>
2023-02-21 22:17:08 +0100

    Remove sessions

--- packages/extension/src/ui/features/accountPlugins.tsx/Plugins.tsx
+++ packages/extension/src/ui/features/accountPlugins.tsx/Plugins.tsx
@@ -1,7 +1,3 @@
-import { SESSION_PLUGIN_CLASS_HASH } from "@argent/x-sessions"
-
-import { SessionPluginIcon } from "../../components/Icons/SessionPluginIcon"
-
 export interface IPlugin {
   classHash: string
   title: string
@@ -10,11 +6,4 @@ export interface IPlugin {
 }
 
 export const plugins: IPlugin[] = [
-  {
-    classHash: SESSION_PLUGIN_CLASS_HASH,
-    title: "User sessions",
-    description:
-      "In supported dapps, you will be able to allow the dapp to perform certain transactions on your behalf without needing further confirmation",
-    icon: <SessionPluginIcon />,
-  },
 ]
--- packages/sessions/src/main.ts
+++
@@ -1,3 +0,0 @@
-export * from "./utils"
-export { SessionAccount } from "./account"
-export { SessionAccount as default } from "./account"
