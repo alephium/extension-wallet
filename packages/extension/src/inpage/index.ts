b378634e54c57695ded71a4bfa198ac3fe2c14b8
Cheng Wang <wachmc@gmail.com>
2023-03-01 20:20:50 +0100

    Auto disconnect accounts for dapp

--- packages/extension/src/inpage/index.ts
+++ packages/extension/src/inpage/index.ts
@@ -59,56 +59,18 @@ function attach() {
 // inject script
 attach()
 
-// TODO: check if this listener is necessary
 window.addEventListener(
   "message",
   async ({ data }: MessageEvent<WindowMessageType>) => {
-    // const { alephiumProviders } = window
-    // const alephium = alephiumProviders?.alephium
+    const { alephiumProviders } = window
+    const alephium = alephiumProviders?.alephium
 
-    // if (!alephium) {
-    //   return
-    // }
+    if (!alephium) {
+      return
+    }
 
-    // if (
-    //   data.type === "CONNECT_ACCOUNT_RES" ||
-    //   data.type === "APPROVE_REQUEST_SWITCH_CUSTOM_NETWORK"
-    // ) {
-    //   const account =
-    //     data.type === "CONNECT_ACCOUNT_RES"
-    //       ? data.data
-    //       : data.data.selectedAccount
-
-    //   const isPreauthorized = await getIsPreauthorized()
-    //   if (!isPreauthorized) {
-    //     // disconnect so the user can see they are no longer connected
-    //     // TODO: better UX would be to also re-connect when user selects pre-authorized account
-    //     await disconnectAccount()
-    //   } else {
-    //     const walletAccountP = waitForMessage(
-    //       "CONNECT_DAPP_RES",
-    //       10 * 60 * 1000,
-    //     )
-    //     sendMessage({
-    //       type: "CONNECT_DAPP",
-    //       data: { host: window.location.host },
-    //     })
-    //     const walletAccount = await walletAccountP
-
-    //     if (!walletAccount) {
-    //       return disconnectAccount()
-    //     }
-
-    //     if (
-    //       account &&
-    //       (account.address !== alephium.connectedAccount?.address ||
-    //         account.networkId !== alephium.connectedNetworkId)
-    //     ) {
-    //       await alephium.disconnect()
-    //     }
-    //   }
-    // } else if (data.type === "DISCONNECT_ACCOUNT") {
-    //   await alephium.disconnect()
-    // }
+    if (data.type === "DISCONNECT_ACCOUNT") {
+      await alephium.disconnect()
+    }
   },
 )
