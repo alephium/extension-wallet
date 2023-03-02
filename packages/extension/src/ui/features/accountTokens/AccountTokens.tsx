3ba324bdbafff6e9872147c2b71016e3c352d6ac
Cheng Wang <wachmc@gmail.com>
2023-03-02 16:35:28 +0100

    Simplify account screen

--- packages/extension/src/ui/features/accountTokens/AccountTokens.tsx
+++ packages/extension/src/ui/features/accountTokens/AccountTokens.tsx
@@ -1,21 +1,12 @@
 import { CellStack } from "@argent/ui"
 import { Flex, VStack } from "@chakra-ui/react"
-import { FC, useCallback, useEffect, useRef } from "react"
-import { useLocation, useNavigate } from "react-router-dom"
-import useSWR from "swr"
+import { FC } from "react"
 
-import { useKeyValueStorage } from "../../../shared/storage/hooks"
-import { userReviewStore } from "../../../shared/userReview"
-import { getAccountIdentifier } from "../../../shared/wallet.service"
-import { routes } from "../../routes"
-import { withPolling } from "../../services/swr"
 import { Account } from "../accounts/Account"
 import {
   getAccountName,
   useAccountMetadata,
 } from "../accounts/accountMetadata.state"
-import { useAccountTransactions } from "../accounts/accountTransactions.state"
-import { useCurrentNetwork } from "../networks/useNetworks"
 import { useBackupRequired } from "../recovery/backupDownload.state"
 import { RecoveryBanner } from "../recovery/RecoveryBanner"
 import { StatusMessageBannerContainer } from "../statusMessage/StatusMessageBanner"
@@ -23,8 +14,6 @@ import { AccountTokensButtons } from "./AccountTokensButtons"
 import { AccountTokensHeader } from "./AccountTokensHeader"
 import { TokenList } from "./TokenList"
 import { useCurrencyDisplayEnabled } from "./tokenPriceHooks"
-import { useFeeTokenBalance } from "./tokens.service"
-import { UpgradeBanner } from "./UpgradeBanner"
 import { useAccountStatus } from "./useAccountStatus"
 
 interface AccountTokensProps {
@@ -32,30 +21,12 @@ interface AccountTokensProps {
 }
 
 export const AccountTokens: FC<AccountTokensProps> = ({ account }) => {
-  const navigate = useNavigate()
   const status = useAccountStatus(account)
   const { accountNames } = useAccountMetadata()
   const { isBackupRequired } = useBackupRequired()
   const currencyDisplayEnabled = useCurrencyDisplayEnabled()
-  const transactionsBeforeReview = useKeyValueStorage(
-    userReviewStore,
-    "transactionsBeforeReview",
-  )
-
-  const userHasReviewed = useKeyValueStorage(userReviewStore, "hasReviewed")
 
   const accountName = getAccountName(account, accountNames)
-
-  useEffect(() => {
-    let timeoutId: ReturnType<typeof setTimeout>
-    // Disable review
-    /*
-    if (!userHasReviewed && transactionsBeforeReview === 0) {
-      timeoutId = setTimeout(() => navigate(routes.userReview()), 1000)
-    }
-    */
-    return () => timeoutId && clearTimeout(timeoutId)
-  }, [navigate, transactionsBeforeReview, userHasReviewed])
 
   const showBackupBanner = isBackupRequired
 
