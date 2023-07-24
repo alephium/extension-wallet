bf1b77a8442cabc5de6f35a5352be585ce5ad68b
h0ngcha0 <lbs.lhc@gmail.com>
2023-07-25 00:28:01 +0200

    Fix max button when sending token with decimal 0

--- packages/extension/src/ui/features/accountTokens/tokens.service.ts
+++ packages/extension/src/ui/features/accountTokens/tokens.service.ts
@@ -23,20 +23,22 @@ const formatTokenBalanceToCharLength =
     (balance: BigNumberish = 0, decimals = 18): string => {
       const balanceBn = BigNumber.from(balance)
       const balanceFullString = utils.formatUnits(balanceBn, decimals)
-
       // show max ${length} characters or what's needed to show everything before the decimal point
       const balanceString = balanceFullString.slice(
         0,
         Math.max(length, balanceFullString.indexOf(".")),
       )
 
-      // make sure seperator is not the last character, if so remove it
-      // remove unnecessary 0s from the end, except for ".0"
       let cleanedBalanceString = balanceString
-        .replace(/\.$/, "")
-        .replace(/0+$/, "")
-      if (cleanedBalanceString.endsWith(".")) {
-        cleanedBalanceString += "0"
+      if (decimals > 0) {
+        // make sure seperator is not the last character, if so remove it
+        // remove unnecessary 0s from the end, except for ".0"
+        cleanedBalanceString = balanceString
+          .replace(/\.$/, "")
+          .replace(/0+$/, "")
+        if (cleanedBalanceString.endsWith(".")) {
+          cleanedBalanceString += "0"
+        }
       }
 
       return cleanedBalanceString
